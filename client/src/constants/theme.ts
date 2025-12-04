6fd3eec4ac5275e90a14cf1f984dc115e52cea40
HarshvardhanNC <harshvardhanchinchkhedkar@gmail.com>
2025-12-04 18:25:51 +0530

    setup auth pages, global stores and axios/firebase config also color theme

---
+++ client/src/constants/theme.ts
@@ -0,0 +1,16 @@
+export const THEME = {
+    colors: {
+      primary: {
+        DEFAULT: "#0F2B5B",
+        light: "#1C3D74",
+      },
+      accent: {
+        DEFAULT: "#9D1C20",
+        light: "#B3202A",
+      },
+      background: "#F8F8F8",
+      textDark: "#1A1A1A",
+      textLight: "#6B7280",
+      border: "#E5E7EB",
+    },
+  } as const;
